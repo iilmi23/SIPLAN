773c81c79601435c464662b2b38fd8524b4c5704
FRNDZNE <hafizptr.14@gmail.com>
2026-06-25 08:40:51 +0700

    generate issue can't build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    // Tambahkan baris base di bawah ini agar vite tahu path aplikasinya
-    base: '/SIPLAN/public/',
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: ["resources/css/app.css", "resources/js/app.tsx"],
             refresh: true,
         }),
         react(),
